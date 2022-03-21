751b794b142d91553654b696843b8aa3918fa1e8
bob <zbo2@qq.com>
2022-03-21 10:41:44 +0800

    add high sheet

--- gen_category.py
+++ gen_category.py
@@ -8,7 +8,7 @@ filein = '/Users/zhubo/Documents/中美对话周期.xlsx'
 fileout = '/Users/zhubo/Documents/out.xlsx'
 wb_out = Workbook()
 wb = load_workbook(filename=filein)
-sheet_ranges = wb['虎年高度']
+high_sheet = wb['虎年高度']
 category = []
 existing = []
 others = []
@@ -21,10 +21,10 @@ else:
 
 
 def get_categoty():
-    global category, cate, index
+    index = 2
     category = set()
-    while sheet_ranges['c{0}'.format(index)].value is not None:
-        cate = sheet_ranges['c{0}'.format(index)].value
+    while high_sheet['c{0}'.format(index)].value is not None:
+        cate = high_sheet['c{0}'.format(index)].value
         category.add(cate)
         index = index + 1
     return category
@@ -57,12 +57,13 @@ def copy_row(target_sheet, source_sheet, index):
     target_row = target_sheet.max_row+1
     for c in range(1, source_sheet.max_column+1):
         target_cell = target_sheet.cell(row=target_row, column=c)
+        # print('index={0} column={1}'.format(index,c))
         source_cell = source_sheet.cell(row=index, column=c)
         target_cell.value = source_cell.value
         target_cell.fill = PatternFill("solid", fgColor=source_cell.fill.fgColor)
         thin = Side(border_style="thin", color="000000")
         target_cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
-        if target_cell.column_letter not in ['A','B','C']:
+        if target_cell.column_letter not in ['A', 'B', 'C']:
             target_sheet.column_dimensions[target_cell.column_letter].width = 3
 
 
@@ -76,27 +77,64 @@ def copy_title(target_sheet, source_sheet):
         target_cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
 
-if __name__ == '__main__':
+def generate_cate_sheet():
+    global index, category, others, cate, name
     index = 2
+    while high_sheet['c{0}'.format(index)].value is not None:
+        cate = high_sheet['c{0}'.format(index)].value
+        fix_cate = get_cate(cate)
+        copy_row(wb_out[fix_cate], high_sheet, index)
+        index = index + 1
+    for name in wb_out.sheetnames:
+        copy_title(wb_out[name], high_sheet)
+    sheet1 = wb_out['Sheet']
+    wb_out.remove(sheet1)
+    wb_out.save(fileout)
+
+
+def gen_sheet():
+    for item in existing:
+        if '其他' not in wb_out.sheetnames:
+            wb_out.create_sheet('其他')
+        if '高度' not in wb_out.sheetnames:
+            wb_out.create_sheet('高度')
+        if item[0] not in wb_out.sheetnames:
+            wb_out.create_sheet(title=item[0])
+    wb_out.save(fileout)
+
+
+def meet_high_condition(source_sheet, index):
+    color = []
+    for c in range(1, source_sheet.max_column + 1):
+        source_cell = source_sheet.cell(row=index, column=c)
+        color.append(source_cell.fill.fgColor)
+    max_num = 0
+    total_num = 0
+    for color_item in color:
+        if color_item.index == 'FFFF0000':
+            total_num = total_num + 1
+        else:
+            if total_num>max_num:
+                max_num = total_num
+            total_num = 0
+    return max_num >= 5
+
+
+def generate_top_sheet():
+    for i in range(1, high_sheet.max_row):
+        if i == 1:
+            copy_title(wb_out['高度'], high_sheet)
+        else:
+            if meet_high_condition(high_sheet, i):
+                copy_row(wb_out['高度'], high_sheet, i)
+    wb_out.save(fileout)
+
+
+if __name__ == '__main__':
     category = get_categoty()
     # print(category)
     others = get_others()
     # print(others)
-    for item in existing:
-        if '其他' not in wb_out.sheetnames:
-            ws = wb_out.create_sheet('其他')
-        if item[0] not in wb_out.sheetnames:
-            ws = wb_out.create_sheet(title=item[0])
-    wb_out.save(fileout)
-
-    index = 2
-    while sheet_ranges['c{0}'.format(index)].value is not None:
-        cate = sheet_ranges['c{0}'.format(index)].value
-        fix_cate = get_cate(cate)
-        copy_row(wb_out[fix_cate], sheet_ranges, index)
-        index = index + 1
-    for name in wb_out.sheetnames:
-        copy_title(wb_out[name],sheet_ranges)
-    sheet1 = wb_out['Sheet']
-    wb_out.remove(sheet1)
-    wb_out.save(fileout)
+    gen_sheet()
+    generate_cate_sheet()
+    generate_top_sheet()

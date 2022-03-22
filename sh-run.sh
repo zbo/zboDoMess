796532146a83a60017e37ee71864921c8b0abc7c
bob <zbo2@qq.com>
2022-03-22 14:15:53 +0800

    remove no use bash

--- sh-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#python2.7 gen.py >README.md
-rm -f ./history.xlsx
-#rm -f ./list.csv
-#python process_raw.py > list.csv
-#python3 sina_batch.py
-python ./history_netease.py
-open .
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 python3 gen_category.py
 python3 sina_batch.py
 python3 real_update.py
+open ~/Documents/out.xlsx

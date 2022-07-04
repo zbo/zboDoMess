443a33a410c2a3c9bd7de137409e281b96a7090c
bob <zbo2@qq.com>
2022-07-04 15:32:26 +0800

    save

--- gen_pyweb.py
+++ gen_pyweb.py
@@ -8,12 +8,16 @@ image_template = '<figure class="third"><img src="http://image.sinajs.cn/newchar
 link_temp1 = '<a href="http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_AllNewsStock/symbol/{0}.phtml">消息面</a>'
 link_temp2 = '<p><a href="http://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpOtherInfo/stockid/{0}/menu_num/5.phtml">基本面</a>'
 link_taogu = '<a herf="https://www.taoguba.com.cn/quotes/{0}"></a>'
+sz_dynamic = '<a href="http://www.szse.cn/disclosure/supervision/dynamic/">深交所</a>'
+sh_dynamic = '<a href="http://www.sse.com.cn/disclosure/credibility/supervision/measures/">上交所</a>'
 jquery = '<script src="./jquery.js"></script>'
 chartjs = '<script src="./chart.js"></script>'
 cate_dict = comlib.get_cate_dict_from_xl()
 print(jquery)
 print(chartjs)
 print('<button id="btn_show1">Show me the money</button>')
+print(sz_dynamic)
+print(sh_dynamic)
 print('<p id="result1"></p>')
 with open('reduced.csv', 'r') as f:
     reader = csv.reader(f)
@@ -33,7 +37,8 @@ with open('reduced.csv', 'r') as f:
         if row[0] in cate_dict:
             cate = cate_dict[row[0]]
         print('<a>{0}</a>'.format(cate))
-        print('<input id ="{0}" type="checkbox"/></p>'.format(code))
+        print('<input id ="{0}" type="checkbox"/>'.format(code))
+        #print('<input id="Radio1" name="group01" type="radio" /><label for="Radio1">多</label><input id="Radio2" name="group01" type="radio" /><label for="Radio2">空</label></p>')
         print(image_template.format(code, code))
 print('<p id="result2"></p>')
 print('<button id="btn_show2">Show me the money</button>')
